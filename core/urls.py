a4c639268258e68fd962a30403cc51acb93d16a0
Abhinav Singh <abhinavsingh@Abhinavs-MacBook-Pro.local>
2026-05-10 14:43:35 +0530

    feat: add home page with expenses tile

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,12 @@
+from django.shortcuts import render
 from django.urls import path, include
 
+
+def home(request):
+    return render(request, "home.html")
+
+
 urlpatterns = [
+    path("", home, name="home"),
     path("expenses/", include("expenses.urls")),
 ]
